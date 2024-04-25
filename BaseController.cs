62a47bf95a07bd9c07ca6b0ab5e31e97a003a5cd
viktorkolev555 <viktor.kolev555@m2mservices.com>
2024-04-25 14:45:19 +0300

    Update configuration

--- BaseController.cs
+++ BaseController.cs
@@ -25,4 +25,21 @@ public class BaseController : Controller
         }
         return Ok(myDeviceId);
     }
+    [HttpPost("device/update")]
+    public IActionResult UpdateDevice(string IMEI, string serialNo)
+    {
+        var myDeviceId = 0;
+        var dict = new Dictionary<string, int> { { "IMEI1", 1 }, {"IMEI2", 2} };
+        try
+        {
+            myDeviceId = dict["MN01-3IN"];        
+        }
+        catch (Exception ex)
+        {
+            SentrySdk.CaptureException(ex);
+            return BadRequest();
+        }
+        // .... logic
+        return Ok(myDeviceId);
+    }
 }
