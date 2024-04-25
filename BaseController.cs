61a675308c58266cce5012b338ebac14a65a1111
viktorkolev555 <viktor.kolev555@m2mservices.com>
2024-04-25 14:26:34 +0300

    Add Sentry config

---
+++ BaseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp;
+
+[Route("base")]
+public class BaseController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+    [Route("error")]
+    public IActionResult Error()
+    {
+        var myDeviceId = 0;
+        try
+        {
+            var dict = new Dictionary<string, int> { { "MN01-LTE-M", 1 }, {"MN01-V3", 2} };
+            myDeviceId = dict["MN01-3IN"];        
+        }
+        catch (Exception ex)
+        {
+            SentrySdk.CaptureException(ex);
+        }
+        return Ok(myDeviceId);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
+using Sentry;
+
+SentrySdk.Init(options =>
+{
+    // A Sentry Data Source Name (DSN) is required.
+    // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+    // You can set it in the SENTRY_DSN environment variable, or you can set it in code here.
+    options.Dsn = "https://5fe3580b4fc448a5face23b8b3bfbab6@o4507016867545088.ingest.us.sentry.io/4507146837491712";
+
+    // When debug is enabled, the Sentry client will emit detailed debugging information to the console.
+    // This might be helpful, or might interfere with the normal operation of your application.
+    // We enable it here for demonstration purposes when first trying Sentry.
+    // You shouldn't do this in your applications unless you're troubleshooting issues with Sentry.
+    options.Debug = true;
+
+    // This option is recommended. It enables Sentry's "Release Health" feature.
+    options.AutoSessionTracking = true;
+
+    // Enabling this option is recommended for client applications only. It ensures all threads use the same global scope.
+    options.IsGlobalModeEnabled = false;
+
+    // This option will enable Sentry's tracing features. You still need to start transactions and spans.
+    options.EnableTracing = true;
+
+    // Example sample rate for your transactions: captures 10% of transactions
+    options.TracesSampleRate = 0.1;
+    options.StackTraceMode = StackTraceMode.Enhanced;
+});
+
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseWindowsService();
 builder.WebHost.ConfigureKestrel((context, serverOptions) =>
@@ -25,6 +55,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
